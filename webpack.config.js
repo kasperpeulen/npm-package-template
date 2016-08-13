37f61bcc66936363e7b2cbce7c3139468d039653
Kasper Peulen <kasperpeulen@gmail.com>
2016-08-13 20:11:09 +0200

    add tests using mocha and chai

--- webpack.config.js
+++ webpack.config.js
@@ -8,22 +8,7 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ["es2015", "es2016", "babel-preset-react"],
-        plugins: [
-          "syntax-trailing-function-commas",
-          "syntax-async-functions",
-          "transform-class-properties",
-          "transform-object-rest-spread",
-          "transform-regenerator",
-          ["transform-runtime", {
-            helpers: false,
-            polyfill: false,
-            regenerator: true
-          }]
-        ]
-      }
+      loader: 'babel'
     }]
   }
 };
